dab2c48013097a4cc250660f93f8a5f8cb46f663
Doug Enas <douglassenas@gmail.com>
2019-03-21 10:38:19 -0400

    restart

--- dirwatcher.py
+++ dirwatcher.py
@@ -1,44 +1,20 @@
-#! python3
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Dirwatcher program.
+Long Running Program that monitors a directory for certain files and a magic
+word in the directory.
+The program will continue to run unless sent a signal to terminate.
+All other errors will be handled with exceptions and logged and
+the program will continue running.
 
+"""
+__author__ = 'dougenas'
+
+import argparse
+import logging
 import signal
-
+import time
+import datetime
+import os
 
 exit_flag = False
-
-
-def signal_handler(sig_num, frame):
-    """
-    This is a handler for SIGTERM and SIGINT. Other signals can be mapped here as well (SIGHUP?)
-    Basically it just sets a global flag, and main() will exit it's loop if the signal is trapped.
-    :param sig_num: The integer signal number that was trapped from the OS.
-    :param frame: Not used
-    :return None
-    """
-    # log the associated signal name (the python3 way)
-    logger.warn('Received ' + signal.Signals(sig_num).name)
-    # log the signal name (the python2 way)
-    signames = dict((k, v) for v, k in reversed(sorted(signal.__dict__.items()))
-                    if v.startswith('SIG') and not v.startswith('SIG_'))
-    logger.warn('Received ' + signames[sig_num])
-    exit_flag = True
-
-
-def main():
-    # Hook these two signals from the OS ..
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.signal(signal.SIGTERM, signal_handler)
-    # Now my signal_handler will get called if OS sends either of these to my process.
-
-    while not exit_flag:
-        try:
-        # call my directory watching function..
-        except Exception as e:
-        # This is an UNHANDLED exception
-        # Log an ERROR level message here
-
-        # put a sleep inside my while loop so I don't peg the cpu usage at 100%
-        time.sleep(polling_interval)
-
-    # final exit point happens here
-    # Log a message that we are shutting down
-    # Include the overall uptime since program start.
