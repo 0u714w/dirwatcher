d1a272b34727646fe3c9215b9af4525b1c685ed0
Doug Enas <douglassenas@gmail.com>
2019-04-06 15:42:41 -0400

    program meets criteria

--- dirwatcher.py
+++ dirwatcher.py
@@ -18,14 +18,15 @@ import signal
 import time
 import os
 
-exit_flag = True
-checked_files = []
+stay_running = True
+files_logged = []
+found_magic_text = {}
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 formatter = logging.Formatter('%(asctime)s:%(message)s')
-file_handler = logging.FileHandler("test.log")
+file_handler = logging.FileHandler("dirwatcher.log")
 file_handler.setFormatter(formatter)
 
 stream_handler = logging.StreamHandler()
@@ -36,52 +37,63 @@ logger.addHandler(stream_handler)
 
 
 def signal_handler(signum, frame):
-    global exit_flag
+    global stay_running
     sigs = dict((k, v) for v, k in reversed(sorted(signal.__dict__.items()))
                 if v.startswith('SIG') and not v.startswith('SIG_'))
     logger.warning('Received OS Signal: {}'.format(sigs[signum]))
-    if signum == signal.SIGINT or signum == signal.SIGTERM:
-        exit_flag = False
+
+    stay_running = False
 
 
-def find_magic_word(dir, text):
-    # print(os.listdir(path))
+def find_magic_word(directory, magic_text, ext):
+    """Watches a given directory for instances of a magic text"""
+    global files_logged
+    global found_magic_text
 
-    global checked_files
-    abspath = os.path.abspath(dir)
-    files = os.listdir(abspath)
-
-    text_files = [f for f in files]
-    for file in text_files:
-        if file.endswith(".txt") and file not in checked_files:
-            logger.info('New file found: {}'.format(file))
-            checked_files.append(file)
-        if file.endswith(".txt"):
-            file_path = os.path.join(abspath, file)
-            if search_file(file_path, text):
-                break
-        for file in checked_files:
+    try:
+        text_files = [f for f in os.listdir(directory) if not f.startswith('.')]
+    except:
+        logger.info('Directory {} does not exists'.format(directory))
+    else:
+        abspath = os.path.abspath(directory)
+        files = os.listdir(abspath)
+        for file in text_files:
+            if file.endswith(ext) and file not in files_logged:
+                logger.info('New file found: {}'.format(file))
+                files_logged.append(file)
+            if file.endswith(ext):
+                full_path = os.path.join(abspath, file)
+                if search_single_file(full_path, magic_text):
+                    break
+        for file in files_logged:
             if file not in files:
                 logger.info('File deleted: {}'.format(file))
-                checked_files.remove(file)
+                files_logged.remove(file)
+                found_magic_text[file] = 0
 
-def search_file(file, text):
-    logger.info("Searching {} for instances of {}".format(file, text))
 
-    with open(file) as doc:
+                  
+
+def search_single_file(full_pathname, magic_text):
+    """Seatches a signle file for a line containing magic text"""
+    with open(full_pathname) as doc:
         content = doc.readlines()
-        for i, line in enumerate(content):
-            if text in line:
-                logger.info("Match found for {} found on line {} in {}".format(text, i + 1, file))
-        
+        for line_number, line in enumerate(content):
+            if magic_text in line:
+                if full_pathname not in found_magic_text.keys():
+                    found_magic_text[full_pathname] = line_number
+                if (line_number >= found_magic_text[full_pathname]) and full_pathname in found_magic_text.keys():
+                    logger.info("Match found for {} found on line {} in {}".format(magic_text, line_number + 1, full_pathname))
+                    found_magic_text[full_pathname] += 1
 
 
 def create_parser():
     """"creates argument parser"""
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Watches a directory for files containing magic text')
     parser.add_argument('-i', '--int', help='Polling interval for program')
-    parser.add_argument('-m', '--magic', help='Magic text to be found in files')
-    parser.add_argument('-p', '--path', help='Directory to be searched')
+    parser.add_argument('-e', '--ext', help='Extension of file to search for', default=".txt")
+    parser.add_argument('path', help='Directory to be searched', default= ".")
+    parser.add_argument('magic', help='Magic text to be found in files')
     return parser
 
 
@@ -90,20 +102,17 @@ def main():
     parser = create_parser()
     args = parser.parse_args()
 
-    path = args.path
-    text = args.magic
-    int = args.int
-
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
 
     # enter function for finding magic word
-    while exit_flag:
+    logger.info("Watching directory {} for files ending with {} containing magic text {}".format(args.path, args.ext, args.magic))
+    while stay_running:
         try:
-            find_magic_word(path, text)
+            find_magic_word(args.path, args.magic, args.ext)
         except Exception:
             logger.exception("exception on main")
-        time.sleep(float(int))
+        time.sleep(float(args.int))
         logger.info("Program uptime: {} seconds".format(time.time() - start_time))
 
 
